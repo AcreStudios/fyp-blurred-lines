8c4171f76a2b2c5caa3effc9183908569cb1d71d
soulagent <goldenwarrior97@gmail.com>
2016-06-28 02:29:23 +0800

    Our beloved Menu is here
    Menu is in!
    New and improved loading screen! (I just got lazy. You see and then
    you'll know why)
    Cutscene Scene planned to be added! Until then!

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public Button startGame;
+    public Text startText;
+    public Text loadingText;
+
+	// Use this for initialization
+	void Start () {
+        startGame = startGame.GetComponent<Button>();
+        startText = startText.GetComponent<Text>();
+        loadingText = loadingText.GetComponent<Text>();
+
+        startText.enabled = true;
+        loadingText.enabled = false;
+	}
+	
+    public void interact() {
+        startText.enabled = false;
+        loadingText.enabled = true;
+
+        StartCoroutine(TimerThingy());
+    }
+
+    IEnumerator TimerThingy() {
+        yield return new WaitForSeconds(4);
+        Application.LoadLevel("CAMP_INT_Initiation");
+    }
+
+}
