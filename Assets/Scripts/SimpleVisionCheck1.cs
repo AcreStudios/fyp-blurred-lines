ddea9db40781f74c4dbd8633ce981b3049562e55
soulagent <goldenwarrior97@gmail.com>
2016-07-01 18:30:26 +0800

    Fixes
    Enemy AI should glow now

---
+++ Assets/Scripts/SimpleVisionCheck1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SimpleVisionCheck1 : MonoBehaviour 
+{
+	MeshRenderer rend;
+
+	void Awake()
+	{
+		rend = GetComponent<MeshRenderer>();
+		rend.material.shader = Shader.Find("Custom/ObjectHighlight");
+	}
+
+	void Update () 
+	{
+		if(CAM_ElectroVision.visionActive)
+		{
+			rend.material.SetFloat("_UseHighlights", 1);
+		}
+		else
+			rend.material.SetFloat("_UseHighlights", 0);
+	}
+}
