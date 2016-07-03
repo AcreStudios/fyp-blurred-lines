19d78939df4c882bfbc7d42fbab7ec1fc54f9762
soulagent <goldenwarrior97@gmail.com>
2016-07-03 20:58:20 +0800

    Legit Loading Screen
    Swapped out a timed loading scene with a legit loading screen. (YOU'RE A
    PROGRAMMER GAVIN :clap: )
    
    keeding but I felt proud doing it :>
    and also realized that our level takes a legit while to load.

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -9,27 +9,41 @@ public class Menu : MonoBehaviour
     public Button startGame;
     public Text startText;
     public Text loadingText;
+    public Text tipsText;
+    public RawImage BLLogo;
+    public RawImage ZLogo;
 
 	// Use this for initialization
 	void Start () {
         startGame = startGame.GetComponent<Button>();
         startText = startText.GetComponent<Text>();
         loadingText = loadingText.GetComponent<Text>();
+        tipsText = tipsText.GetComponent<Text>();
+        BLLogo = BLLogo.GetComponent<RawImage>();
+        ZLogo = ZLogo.GetComponent<RawImage>();
 
         startText.enabled = true;
         loadingText.enabled = false;
+        tipsText.enabled = false;
+        BLLogo.enabled = true;
+        ZLogo.enabled = false;
 	}
 	
     public void interact() {
         startText.enabled = false;
         loadingText.enabled = true;
+        BLLogo.enabled = false;
+        ZLogo.enabled = true;
+        tipsText.enabled = true;
 
         StartCoroutine(TimerThingy());
     }
 
     IEnumerator TimerThingy() {
-        yield return new WaitForSeconds(4);
-        SceneManager.LoadScene("CAMP_INT_Initiation");
+        //yield return new WaitForSeconds(4);
+        AsyncOperation async = SceneManager.LoadSceneAsync("CAMP_INT_Initiation");
+        yield return async;
+        Debug.Log("Loading done");
     }
 
 }
